e3b752df4c99c20a71f49e61c69a8f0284a63576
Dmitry Senchenko <114693652+DmitrySA1996@users.noreply.github.com>
2023-06-25 20:47:21 +0300

    Add files via upload

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,5 +7,5 @@ const SECRET_SIGNING_KEY = 'f1c81533098ee359576bb24a9df5fbd06c4dfe93e3a23900323c
 const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 module.exports = {
-  OK, CREATED, BAD_REQUEST, NOT_FOUND, SERVER_ERROR, SECRET_SIGNING_KEY, URL_REGEX
+  OK, CREATED, BAD_REQUEST, NOT_FOUND, SERVER_ERROR, SECRET_SIGNING_KEY, URL_REGEX,
 };
