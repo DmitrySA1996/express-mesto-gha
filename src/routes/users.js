e3c518e4a14f3e4a5e3a3f9e5effd336a876cc81
Dmitry Senchenko <114693652+DmitrySA1996@users.noreply.github.com>
2023-06-25 22:54:37 +0300

    Add files via upload

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-
+const { celebrate, Joi } = require('celebrate');
+const URL_REGEX = require('../utils/constants');
 const {
   getUsers,
   getUserId,
@@ -11,8 +12,25 @@ const {
 
 router.get('/', getUsers); // - // получить пользователей
 router.get('/:id', getUserId); // - Поиск пользователя по id
-router.post('/signup', createUser); // - Создать пользователя
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi
+      .string()
+      .pattern(URL_REGEX),
+  }),
+}), createUser); // - Создать пользователя
 router.patch('/me', updateProfile); // - Обновляет профиль
 router.patch('/me/avatar', updateAvatar); // - Обновляет аватар
-router.post('/signin', loginUser);
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), loginUser);
+
 module.exports = router;
