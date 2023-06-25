c45468572287376ab509557623d1b881ba2f7284
Dmitry Senchenko <114693652+DmitrySA1996@users.noreply.github.com>
2023-06-25 23:30:06 +0300

    Add files via upload

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-const { FORBIDDEN } = require('../utils/constants');
+const { NOT_FOUND } = require('../utils/constants');
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 router.use('/*', (req, res) => {
-  res.status(FORBIDDEN)
-    .send({ message: '403: Нет прав доступа.' });
+  res.status(NOT_FOUND)
+    .send({ message: '404: Страница не найдена.' });
 });
 
 module.exports = router;
